fa587d7f323e9765dbdb7d47257166b0ee3be966
Dino Repac <dinorepac93@gmail.com>
2024-10-10 23:08:11 +0200

    Add form

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -1,7 +1,37 @@
 "use client";
 
 import { Form } from "@/components/ui/form";
+import { useAppForm } from "@/hooks/useAppForm";
+import { z } from "zod";
+
+// email & password
+const FormSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+type FormType = z.infer<typeof FormSchema>;
 
 export const LoginForm = () => {
-  return <Form></Form>;
+  const form = useAppForm<FormType>({
+    schema: FormSchema,
+    defaultValues: {
+      email: '',
+      password: '',
+    }
+  });
+
+  const { handleSubmit } = form;
+
+  const onSubmit = (data: FormType) => {
+    console.log('data', data);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* OUR FORM */}
+      </form>
+    </Form>
+  )
 };
